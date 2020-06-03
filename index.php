9e19ec08c6b406e2972bbf04b225e9c2a26b3c11
jumpei arai <jumpei0924@gmail.com>
2020-06-04 00:09:44 +0900

    up

--- index.php
+++ index.php
@@ -1,59 +1,44 @@
 <?php
 
-$array_1 = ['あああ',2,3];
+//複数の値をてんか　foreach
 
-$array_2 = [
-  ['赤','青','黄'],
-  ['緑','紫','黒']
-];
-
-//0から始まる
-// echo $array[0];
-
-echo '<pre>';
-var_dump($array_2);
-echo '</pre>';
-
-echo $array_2[1][1];
-
-$array_member = [
+$members = [
   'name' => '本田',
   'height' => 170,
   'hobby' => 'サッカー'
 ];
 
-echo $array_member['hobby'];
 
-echo '<pre>';
-var_dump($array_member);
-echo '</pre>';
+// バリューのみ表示
+foreach($members as $member){
+  echo $member;
+}
 
-$array_member_3 = [
-  '1kumi'=> [
-    '本田'=> [
-      'height' => 170,
-      'hobby' => 'サッカー'
+echo '<br>';
+
+// キーとバリューそれぞれ表示
+foreach($members as $key => $value){
+echo $key .'は'.$value. 'です';
+}
+
+$members_2 = [
+  '本田' => [
+  'height' => 170,
+  'hobby' => 'サッカー'
   ],
-  '香川'=> [
-    'height' => 165,
-    'hobby' => 'サッカー'
-   ]
-  ],
-  '2kumi' => [
-    '長友' => [
-    'height' => 160,
-    'hobby' => 'サッカー'
-    ],
-    '乾'=> [
-      'height' => 168,
-      'hobby' => 'サッカー'
-    ]
+  '香川' => [
+  'height' => 165,
+  'hobby' => 'サッカー'
   ]
-];
+  ];
 
-echo $array_member_3['2kumi']['長友']['height'];
+  echo '<br>';
 
-echo '<pre>';
-var_dump($array_member_3);
-echo '</pre>';
+  // 多段階の配列展開
+  foreach($members_2 as $member_1){
+    foreach($member_1 as $member => $value){
+    echo $member . 'は'. $value . 'です';
+    }
+  }
+
 ?>
