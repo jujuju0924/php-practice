a630637ff0511f5ce5a36aa51e2588e529c34179
jumpei arai <jumpei0924@gmail.com>
2020-06-03 21:39:21 +0900

    up

--- index.php
+++ index.php
@@ -28,4 +28,32 @@ echo '<pre>';
 var_dump($array_member);
 echo '</pre>';
 
+$array_member_3 = [
+  '1kumi'=> [
+    '本田'=> [
+      'height' => 170,
+      'hobby' => 'サッカー'
+  ],
+  '香川'=> [
+    'height' => 165,
+    'hobby' => 'サッカー'
+   ]
+  ],
+  '2kumi' => [
+    '長友' => [
+    'height' => 160,
+    'hobby' => 'サッカー'
+    ],
+    '乾'=> [
+      'height' => 168,
+      'hobby' => 'サッカー'
+    ]
+  ]
+];
+
+echo $array_member_3['2kumi']['長友']['height'];
+
+echo '<pre>';
+var_dump($array_member_3);
+echo '</pre>';
 ?>
