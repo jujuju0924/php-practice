bb5d103ef58afc3b5804a477acee4bd3ffb5ce9b
jumpei arai <jumpei0924@gmail.com>
2020-06-09 18:49:29 +0900

    up

--- mainte/test.php
+++ mainte/test.php
@@ -1,10 +1,5 @@
 <?php
 
 
-echo __FILE__;
-// /Applications/MAMP/htdocs/php_test/mainte/test.php
+$contactFile = '.contact.dat';
 
-echo '<br>';
-echo(password_hash('password123',PASSWORD_BCRYPT));
-
-?>
